c167c2d882757ebe5903331fcefa0bdcf212a206
寒浔 <louxinhao.lxh@alibaba-inc.com>
2021-11-11 11:41:57 +0800

    :art: format of the code

--- src/main/java/com/hanxun/demo/config/WebConfig.java
+++ src/main/java/com/hanxun/demo/config/WebConfig.java
@@ -16,11 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/**")// 所有的当前站点的请求地址，都支持跨域访问。
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 当前站点支持的跨域请求类型是什么。
-                .allowCredentials(true) // 是否支持跨域用户凭证
-                .allowedOriginPatterns("*") // 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
-                .maxAge(3600); // 超时时长设置为1小时。 时间单位是秒。
+            .addMapping("/**")// 所有的当前站点的请求地址，都支持跨域访问。
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // 当前站点支持的跨域请求类型是什么。
+            .allowCredentials(true) // 是否支持跨域用户凭证
+            .allowedOriginPatterns("*") // 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
+            .maxAge(3600); // 超时时长设置为1小时。 时间单位是秒。
     }
 
     @Override
