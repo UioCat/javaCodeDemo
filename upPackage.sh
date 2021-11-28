da2cc10dedb6cf46a659e6b6a3b79878e308ed1d
hanxun <406453373@qq.com>
2021-11-28 11:41:30 +0800

    :hammer:update development script

--- upPackage.sh
+++ upPackage.sh
@@ -1,4 +1,24 @@
-# 打包上传脚本
+#!/bin/bash
+# 服务器密码
+PASSWORD=
+
 mvn clean install -Dmaven.test.skip=true
-mv ./target/xxxxx.war ./target/xxxx.war
-scp ./target/xxxx.war root@116.62.4.111:/usr/java/apache-tomcat-9.0.33/webapps
+mv ./target/*.war ./target/demo.war
+
+/usr/bin/expect <<EOF
+set IP_ARRAY {192.168.31.102 192.168.31.103}
+foreach IP \$IP_ARRAY {
+  spawn scp ./target/monitor.war root@\$IP:/usr/java/apache-tomcat-9.0.33/webapps
+  expect {
+   "(yes/no)?" {
+     send "yes\n"
+     expect "*assword:" { send "$PASSWORD\r"}
+    }
+    "*assword:" {
+     send "$PASSWORD\r"
+    }
+  }
+  expect 100%
+  expect eof
+}
+EOF
